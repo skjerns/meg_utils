88687697f467b477bb330b79437fb19d8e19b657
Simon Kern <14980558+skjerns@users.noreply.github.com>
2026-01-16 11:08:42 +0100

    add more stub imports

---
+++ decoding.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 14:18:19 2026
+
+Stub to allow dot imports
+
+@author: simon.kern
+"""
+
+from .meg_utils.decoding import *
---
+++ sigproc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 16 11:06:28 2026
+
+@author: simon.kern
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 14:18:19 2026
+
+Stub to allow dot imports
+
+@author: simon.kern
+"""
+
+from .meg_utils.sigproc import *
